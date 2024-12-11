dc022a75e1155b122b75deaf437ed2ae63661039
sv742744 <shashi09op@gmail.com>
2024-12-11 16:23:13 +0530

    Add files via upload

---
+++ static1.java
@@ -0,0 +1,8 @@
+class static1{
+        public static void s(){
+            System.out.println("hello sumanthi");
+        }
+        public static void main(String []args){
+            s();
+        }
+}
