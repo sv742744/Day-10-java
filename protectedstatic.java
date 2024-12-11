5bc04c75aa6eb81b35c0d0a65274f2be567d57f9
sv742744 <shashi09op@gmail.com>
2024-12-11 16:22:32 +0530

    Add files via upload

---
+++ protectedstatic.java
@@ -0,0 +1,9 @@
+// protected static//
+class protectedstatic{
+        public static void s(){
+            System.out.println("protected static");
+        }
+        public static void main(String []args){
+            s();
+        }
+}
