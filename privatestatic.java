d2e6a22ed5be5af2af8e5f00549acec0a830eef0
sv742744 <shashi09op@gmail.com>
2024-12-11 16:22:17 +0530

    Add files via upload

---
+++ privatestatic.java
@@ -0,0 +1,9 @@
+// privatestatic//
+class privatestatic{
+        public static void s(){
+            System.out.println("private  static");
+        }
+        public static void main(String []args){
+            s();
+        }
+}
