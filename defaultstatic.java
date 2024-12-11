5537e4853851bb2e74aea4f91ff71a5f1c97da84
sv742744 <shashi09op@gmail.com>
2024-12-11 16:20:07 +0530

    Add files via upload

---
+++ defaultstatic.java
@@ -0,0 +1,9 @@
+//default static//
+class defaultstatic{
+        public static void s(){
+            System.out.println("default static");
+        }
+        public static void main(String []args){
+            s();
+        }
+}
