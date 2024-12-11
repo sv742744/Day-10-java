728253c643939fe265bb0a76660c1d87a2f887f8
sv742744 <shashi09op@gmail.com>
2024-12-11 16:22:02 +0530

    Add files via upload

---
+++ nonvoidstatic.java
@@ -0,0 +1,10 @@
+// non void static//
+class nonvoidstatic{
+        public static String s(){
+            return "non void static";
+        }
+        public static void main(String []args){
+            String res=s();
+            System.out.println(res);
+        }
+}
